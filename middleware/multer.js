74f5527f85398fbe8beba655dc4aa157ed1d848c
Titah Baudwin <baudwin12@gmail.com>
2024-03-26 18:26:25 +0100

    vercel configurations

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,13 +1,19 @@
 const multer = require('multer')
-
+try {
 const storage = multer.diskStorage({
-    destination: 'public/uploads/',
+    // destination: 'public/uploads/',
     filename:(req,file,cb)=>{
         cb(null, Date.now() + "_" + file.originalname)
     }
 })
- 
 
-exports.upload = multer({
+const upload = multer({
     storage:storage
 })
+
+module.exports = upload
+
+    
+} catch (err) {
+    console.log(err);
+}
