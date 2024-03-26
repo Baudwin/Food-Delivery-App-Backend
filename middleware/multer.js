55ddcbec89a02a35a78f884163a143336b630e1a
Titah Baudwin <baudwin12@gmail.com>
2024-03-26 16:27:21 +0100

    vercel configurations

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,6 +1,5 @@
 const multer = require('multer')
 
-
 const storage = multer.diskStorage({
     destination: 'public/uploads/',
     filename:(req,file,cb)=>{
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const {authAdmin, addCategory, getCategories, addItems,getItem,deleteItem, getItems, login}  = require('../controllers/AdminController')
 const {validateToken,  validateAdmin} = require('../middleware/JWT')
-const {upload} = require('../middleware/multer')
+// const {upload} = require('../middleware/multer')
 
 router.post("/admin-login", login)
 router.get("/auth-admin",authAdmin)
@@ -9,7 +9,7 @@ router.get("/auth-admin",authAdmin)
 router.post("/create-category", addCategory)
 router.get("/get-categories", getCategories)
 
-router.post("/add-item",upload.single("img"), addItems)
+// router.post("/add-item",upload.single("img"), addItems)
 router.get("/get-items", getItems)
 router.get("/get-item/:id", getItem)
 router.delete("/delete-item/:it_id", deleteItem)
