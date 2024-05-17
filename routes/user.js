ef1a28d88fa0a65c0a1eab42293ea6c22a22658d
Titah Baudwin <baudwin12@gmail.com>
2024-05-17 19:58:17 +0100

    cloudinary bug fixes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const {login, signup, placeOrder, addAddress, getAddress, getUserOrders, oauthSuccess}  = require('../controllers/UserController')
-// const passport = require('passport')
-// require('../Strategies/JwtStragegy')
+const passport = require('passport')
+require('../Strategies/JwtStragegy')
 // require('../Strategies/GoogleStrategy')
 
-// const authenticateJWT = require('../middleware/authenticateJwt')
+const authenticateJWT = require('../middleware/authenticateJwt')
 // const { createToken } = require('../middleware/JWT')
 
 
