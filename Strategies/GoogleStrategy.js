4b4f9d906fe326ed2bad54073b6510ac963dc131
Titah Baudwin <baudwin12@gmail.com>
2024-05-17 20:44:48 +0100

    cloudinary bug fixes

--- Strategies/GoogleStrategy.js
+++ Strategies/GoogleStrategy.js
@@ -1,37 +1,37 @@
 const passport = require('passport')
 require('dotenv').config()
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/UserModel');
 
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
-    session:false,
-     proxy: true,
-  },
- async function(accessToken, refreshToken, profile, cb) {
+// passport.use(new GoogleStrategy({
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL: "/auth/google/callback",
+//     session:false,
+//      proxy: true,
+//   },
+//  async function(accessToken, refreshToken, profile, cb) {
 
-    try {
-        const user  = await User.findOne({ googleId: profile.id})
-       if (!user) {
-           const newUser = await User.create({
-               googleId: profile.id, 
-               username: profile.displayName,
-               email : profile._json.email,
-               img : profile._json.picture
-           })
+//     try {
+//         const user  = await User.findOne({ googleId: profile.id})
+//        if (!user) {
+//            const newUser = await User.create({
+//                googleId: profile.id, 
+//                username: profile.displayName,
+//                email : profile._json.email,
+//                img : profile._json.picture
+//            })
    
-           return cb(null, newUser)
-       }
-       else{
+//            return cb(null, newUser)
+//        }
+//        else{
  
-           return cb(null, user)
-       }  
-       } catch (error) {
-        console.log(error);
-       }
+//            return cb(null, user)
+//        }  
+//        } catch (error) {
+//         console.log(error);
+//        }
 
-  }
-));
+//   }
+// ));
